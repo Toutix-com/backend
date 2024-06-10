bbb8db9452c985c87c03c5ed2e6cb16a65f46371
dxmaptin <91907332+dxmaptin@users.noreply.github.com>
2024-06-10 18:48:07 +0100

    scanner

--- app/api/scanner_app.py
+++ app/api/scanner_app.py
@@ -13,12 +13,17 @@ def ticket_validate(event_id):
 
     # Validate the ticket
     ticket = Ticket.query.filter_by(TicketID=ticket_id, UserID=user_id, EventID=event_id, CategoryID=category_id).first()
+    
     if ticket:
+        user_name = User.query.get(user_id).FirstName + ' ' + User.query.get(user_id).LastName
+        event = Event.query.get(event_id).Name
+        location = Location.query.get(Event.query.get(event_id).LocationID).Name
+        ticket_category = TicketCategory.query.get(category_id).Name
         if not ticket.QR_STATUS:
             ticket.QR_STATUS = True
             db.session.commit()
-            return jsonify({'message': 'Ticket is valid'}), 200
+            return jsonify({'message': 'Ticket is valid', 'user_name': user_name, 'event': event, 'location': location, 'ticket_category': ticket_category}), 200
         else:
-            return jsonify({'message': 'Ticket has already been validated'}), 400
+            return jsonify({'message': 'Ticket has already been validated', 'user_name': user_name, 'event': event, 'location': location, 'ticket_category': ticket_category}), 400
     else:
         return jsonify({'message': 'Ticket not found'}), 404
